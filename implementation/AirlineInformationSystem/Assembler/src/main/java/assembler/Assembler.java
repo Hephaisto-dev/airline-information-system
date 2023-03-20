e5879d1c1fd092946e523694e080559d23e224c3
dRekers <d.rekers@student.fontys.nl>
2023-03-20 12:31:41 +0100

    feedback to action
    - changed folder names from "org.example" to whatever the layer is
    - replaced "DataDictinoary" with "DataRecords"

--- implementation/AirlineInformationSystem/Assembler/src/main/java/assembler/Assembler.java
+++ implementation/AirlineInformationSystem/Assembler/src/main/java/assembler/Assembler.java
@@ -1,7 +1,6 @@
-package org.example;
+package assembler;
 
 public class Assembler {
-
     public static void main(String args[]){
         System.out.println("Assembler running / ran");
     }
--- implementation/AirlineInformationSystem/DataRecords/src/main/java/datarecords/Main.java
+++ implementation/AirlineInformationSystem/DataRecords/src/main/java/datarecords/Main.java
@@ -1,4 +1,4 @@
-package org.example;
+package datarecords;
 
 public class Main {
     public static void main(String[] args) {
