591fa30b337d5654a83e77f7e5be6f12af2afba9
Jazziki17 <jazzfel2222@gmail.com>
2023-03-22 14:02:22 +0100

    j

---
+++ implementation/BusinessLogic/src/test/java/businesslogic/implementation/AirplanerImplTest.java
@@ -0,0 +1,35 @@
+package businesslogic.implementation;
+
+import org.assertj.core.api.SoftAssertions;
+import org.junit.jupiter.api.Test;
+
+class AirplanerImplTest {
+
+    private AirplaneImpl Airplane = new AirplaneImpl("Id", "type", 2);
+
+    @Test
+    void getId() {
+        SoftAssertions.assertSoftly(softly->{
+            softly.assertThat(Airplane.getId())
+                    .isEqualTo("Id");
+
+        });
+    }
+
+    @Test
+    void getPlaneType() {
+        SoftAssertions.assertSoftly(softly->{
+            softly.assertThat(Airplane.getPlaneType())
+                    .isEqualTo("type");
+        });
+    }
+
+    @Test
+    void getMaxCapacity() {
+        SoftAssertions.assertSoftly(softly->{
+            softly.assertThat(Airplane.getMaxCapacity())
+                    .isEqualTo(2);
+        });
+    }
+
+}
