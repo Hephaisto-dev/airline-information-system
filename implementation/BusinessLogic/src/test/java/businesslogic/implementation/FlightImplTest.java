cf252dc8359c4b80317aae00cf719515bd419e42
Hephaisto-dev <hephaistopl@gmail.com>
2023-03-23 09:11:31 +0100

    Review FlightImplTest.java and set field as final. Please combine the test into one for each attributes and test only the flight implementation. FlightImpl objects must me tested !!

--- implementation/BusinessLogic/src/test/java/businesslogic/implementation/FlightImplTest.java
+++ implementation/BusinessLogic/src/test/java/businesslogic/implementation/FlightImplTest.java
@@ -1,46 +1,41 @@
 package businesslogic.implementation;
 
+import businesslogic.api.airplane.Airplane;
 import businesslogic.api.airport.AirportFactory;
+import businesslogic.api.flight.Flight;
 import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.Test;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
-class  FlightImplTest {
+class FlightImplTest {
 
-    private String from = "DEPART";
-    private String to = "ARRIVE";
-    private LocalDateTime LDTd = LocalDateTime.of(2012, 12, 11, 5, 3);
-    private LocalDateTime LDTa = LocalDateTime.of(2012, 12, 11, 8, 23);
-    private LocalDateTime LDTd2 = LocalDateTime.of(2012, 12, 15, 12, 34);
-    private LocalDateTime LDTa2 = LocalDateTime.of(2012, 12, 15, 15, 45);
-    private Duration dur = Duration.between(LDTd, LDTa);
+    private final String from = "DEPART";
+    private final String to = "ARRIVE";
+    private final LocalDateTime LDTd = LocalDateTime.of(2012, 12, 11, 5, 3);
+    private final LocalDateTime LDTa = LocalDateTime.of(2012, 12, 11, 8, 23);
+    private final LocalDateTime LDTd2 = LocalDateTime.of(2012, 12, 15, 12, 34);
+    private final LocalDateTime LDTa2 = LocalDateTime.of(2012, 12, 15, 15, 45);
+    private final Duration dur = Duration.between(LDTd, LDTa);
     //String, String, LocalDateTime, LocalDateTime, Airplane
-    private Duration dur2 = Duration.between(LDTd2, LDTa2);
-    private AirplaneImpl plane = new AirplaneImpl("Hello", "There", 3);
-    private AirplaneImpl plane2 = new AirplaneImpl("Identification", "please", 123);
-
+    private final Duration dur2 = Duration.between(LDTd2, LDTa2);
+    private final Airplane plane = new AirplaneImpl("Hello", "There", 3);
+    private final Airplane plane2 = new AirplaneImpl("Identification", "please", 123);
     //String, String, LocalDateTime, LocalDateTime, Duration, Airplane
-    private AirplaneImpl plane3 = new AirplaneImpl("Wherever", "you are", 1912);
-    private FlightImpl FlightOne = new FlightImpl(AirportFactory.createAirport(from),
+    private final Airplane plane3 = new AirplaneImpl("Wherever", "you are", 1912);
+    private final Flight FlightOne = new FlightImpl(AirportFactory.createAirport(from),
             AirportFactory.createAirport(to), LDTd, LDTa, dur, plane);
-    private FlightImpl FlightTwo = new FlightImpl(AirportFactory.createAirport(from),
+    private final Flight FlightTwo = new FlightImpl(AirportFactory.createAirport(from),
             AirportFactory.createAirport(to), LDTd, LDTa, plane2);
-    private FlightImpl FlightThree = new FlightImpl(AirportFactory.createAirport("Near"),
+    private final Flight FlightThree = new FlightImpl(AirportFactory.createAirport("Near"),
             AirportFactory.createAirport("Far"), LDTd2, LDTa2, plane3);
-    private FlightImpl TooLongFlight = new FlightImpl(AirportFactory.createAirport(from),
+    private final Flight TooLongFlight = new FlightImpl(AirportFactory.createAirport(from),
             AirportFactory.createAirport(to), LDTd, LDTa2, plane2);
-
-    /*
-    SoftAssertions.assertSoftly(softly->{
-            softly.assertThat(Route.getId())
-                    .isEqualTo("DEPART");
-        });
-     */
 
     @Test
     void getId() {
@@ -60,13 +55,10 @@ class  FlightImplTest {
 
     // airplane info testing start
     @Test
-        void testGetPlaneId1() {
-            SoftAssertions.assertSoftly(softly -> {
-                softly.assertThat(plane.getId())
-                        .isEqualTo("Hello");
-
-            });
-        }
+    void testGetPlaneId1() {
+        assertThat(plane.getId())
+                .isEqualTo("Hello");
+    }
 
     @Test
     void testGetPlaneName1() {
@@ -83,6 +75,7 @@ class  FlightImplTest {
                     .isEqualTo(3);
         });
     }
+
     @Test
     void testGetPlaneId2() {
         SoftAssertions.assertSoftly(softly -> {
@@ -90,7 +83,6 @@ class  FlightImplTest {
                     .isEqualTo("Identification");
         });
     }
-
 
 
     @Test
@@ -113,7 +105,7 @@ class  FlightImplTest {
     void testGetPlaneId3() {
         SoftAssertions.assertSoftly(softly -> {
             softly.assertThat(plane3.getId())
-                    .isEqualTo("you are");
+                    .isEqualTo("Wherever");
         });
     }
 
@@ -121,7 +113,7 @@ class  FlightImplTest {
     void testGetPlaneName3() {
         SoftAssertions.assertSoftly(softly -> {
             softly.assertThat(plane3.getName())
-                    .isEqualTo("please");
+                    .isEqualTo("you are");
         });
     }
 
@@ -179,7 +171,7 @@ class  FlightImplTest {
 
     @Test
     void getDepartureAirport() {
-        SoftAssertions.assertSoftly(softly->{
+        SoftAssertions.assertSoftly(softly -> {
             softly.assertThat(from)
                     .isEqualTo("DEPART");
         });
@@ -187,14 +179,9 @@ class  FlightImplTest {
 
     @Test
     void getArrivalAirport() {
-        SoftAssertions.assertSoftly(softly->{
+        SoftAssertions.assertSoftly(softly -> {
             softly.assertThat(to)
                     .isEqualTo("ARRIVE");
         });
     }
-
-    @Test
-    void testToString() {
-    }
-
 }
