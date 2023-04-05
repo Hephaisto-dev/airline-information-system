1a74422d1a90791bae7a82f9f7610f580741f011
Jazziki17 <jazzfel2222@gmail.com>
2023-04-05 09:52:40 +0200

    Database

--- implementation/BusinessLogic/src/main/java/businesslogic/api/manager/FlightManager.java
+++ implementation/BusinessLogic/src/main/java/businesslogic/api/manager/FlightManager.java
@@ -7,6 +7,16 @@ import persistence.FlightStorageService;
 
 public class FlightManager extends ManagerImpl<Flight, FlightData> {
     public FlightManager(FlightStorageService storageService) {
+
         super(storageService);
+
+
+    }
+
+    private Flight searchFlight(String searchPhrase) {
+        return getAll().stream()
+                .filter(flight -> flight.getId().contains(searchPhrase))
+                .findFirst()
+                .orElse(null);
     }
 }
