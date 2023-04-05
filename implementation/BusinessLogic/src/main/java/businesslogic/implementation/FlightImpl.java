95f549b6d92af51e4a25d5573bdd728ff7db98d1
dRekers <d.rekers@student.fontys.nl>
2023-04-05 12:23:23 +0200

    storing booked seats in FlightImpl
    * when a seat is booked now, it is checked if on this flight the corresponding seat is already booked and if not, it is "created" and thus stored (to avoid overcrowding our DB)
    * We can also "unbook" seats
    - tested the implementation of all
    * including the error messages that are created

--- implementation/BusinessLogic/src/main/java/businesslogic/implementation/FlightImpl.java
+++ implementation/BusinessLogic/src/main/java/businesslogic/implementation/FlightImpl.java
@@ -1,6 +1,8 @@
 package businesslogic.implementation;
 
 import businesslogic.api.airplane.Airplane;
+import businesslogic.api.airplane.Seat;
+import businesslogic.api.airplane.SeatImpl;
 import businesslogic.api.airport.Airport;
 import businesslogic.api.flight.Flight;
 import businesslogic.api.route.Route;
@@ -8,6 +10,7 @@ import datarecords.FlightData;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 
 public class FlightImpl extends RouteImpl implements Flight {
 
@@ -15,6 +18,8 @@ public class FlightImpl extends RouteImpl implements Flight {
     private final Airplane airplane;
     private final LocalDateTime LDTd;
     private final LocalDateTime LDTa;
+
+    private ArrayList<Seat> bookedSeats;
 
 
     public FlightImpl(Airport from, Airport to, LocalDateTime etdDateTime, LocalDateTime etaDateTime,
@@ -30,6 +35,7 @@ public class FlightImpl extends RouteImpl implements Flight {
         this.airplane = airplane;
         this.LDTd = etdDateTime;
         this.LDTa = etaDateTime;
+        this.bookedSeats = new ArrayList<>();
     }
 
     public FlightImpl(Airport from, Airport to, LocalDateTime etdDateTime, LocalDateTime etaDateTime,
@@ -66,6 +72,51 @@ public class FlightImpl extends RouteImpl implements Flight {
     @Override
     public Route getRoute() {
         return this;
+    }
+
+    @Override
+    public String bookSeat(int column, String row) {
+        if(column > airplane.getLength()){
+            return "Column number exceeding the amount of columns on this plane";
+        }
+        if(row.length() != 1){
+            return "Only one letter allowed as row identification";
+        }
+        char charAt = row.charAt(0);
+        int c = Character.valueOf(charAt);
+        if(c < Character.valueOf('A') || c > Character.valueOf('Z')){
+            return "The row must be identified by a single capital letter of the English Alphabet (A-Z)";
+        }
+        c -= Character.valueOf('A');
+        if(c > airplane.getWidth()-1){
+            return "The row must be identified by a single capital letter of the English Alphabet (A-Z)";
+        }
+        Seat seat = new SeatImpl(charAt,column);
+        boolean booked = false;
+        for(Seat sit: bookedSeats){
+            if(seat.getID().equals(sit.getID())){
+                booked = true;
+                break;
+            }
+        }
+        if(!booked){
+            bookedSeats.add(seat);
+            return "Seat was successfully booked";
+        }else{
+            return "Seat is already booked by someone else";
+        }
+    }
+
+    @Override
+    public String cancelBookedSeat(String targetID) {
+        int size = bookedSeats.size();
+        for(int i = 0; i < size; i++){
+            if(targetID.equals(bookedSeats.get(i).getID())){
+                bookedSeats.remove(i);
+                return "Seat successfully freed for booking once more";
+            }
+        }
+        return "Seat " + targetID + " couldn't be found in the list of booked seats";
     }
 
     @Override
