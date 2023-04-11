08d76261072735a016c72fce11b6b2a14bdeee25
dRekers <d.rekers@student.fontys.nl>
2023-04-11 10:38:35 +0200

    Creating Tickets
    - added GUI component for the creation of a ticket
    - added controller for this GUI
    - created a classt that handles the creation of tickets and returns what the result (failure / success) is

---
+++ implementation/BusinessLogic/src/main/java/businesslogic/api/customer/TicketCreator.java
@@ -0,0 +1,86 @@
+package businesslogic.api.customer;
+
+import businesslogic.api.airplane.Seat;
+import businesslogic.api.airplane.SeatImpl;
+import businesslogic.api.flight.Flight;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TicketCreator {
+
+    private ArrayList errorList = new ArrayList();
+    private boolean errorFound = false;
+
+    public String createTicket(Flight flight, String row, String column, String customer){
+        return ticketCheck(flight, row, column, customer, errorList);
+    }
+    private String ticketCheck(Flight fly, String CHAR, String NUM, String cus, List list){
+        char letter = 'A';
+        int number = 0;
+        Seat sit;
+        //checking, if the flight was selected
+        if(fly == null){
+            error(list, "Please select one of the flights");
+        }
+        //checking the letter information
+        if(CHAR != null){
+            if(CHAR.length() > 1 || CHAR.length() < 1){
+                error(list, "In the column field just add one capital letter of the English Alphabet");
+            }else{
+                letter = CHAR.charAt(0);
+                if(Character.valueOf(letter)<'A' || Character.valueOf(letter)>'Z'){
+                    error(list, "The letter in the column field must be from the English Alphabet");
+                }
+            }
+        }else{
+            error(list, "Please fill in the column field");
+        }
+        //Checking the number information
+        if(NUM != null){
+            try {
+                number = Integer.getInteger(NUM);
+            }catch(Exception e){
+                e.printStackTrace();//to make errormessages more precise
+                error(list, "Row must be a number without decimal point");
+            }
+        }else{
+            error(list, "Please fill in the row field");
+        }
+        //Checking, if there's a customer
+        if(cus == null){
+            error(list, "Please add a customer name");
+        }
+        //Booking the Seat
+        if(!errorFound){
+            if(number > fly.getAirplane().getLength()){
+                error(list, "The selected plane has a length of " + fly.getAirplane().getLength()
+                        + ", you asked for " + number);
+            }else{
+                String a = fly.bookSeat(number, letter);
+                if(!a.contains("successfully")){
+                    error(list, a);
+                }
+            }
+        }
+        //returning the end result
+        if(!errorFound){
+            return "Ticket booked successfully";
+        }
+        return getErrors(list);
+    }
+    public void setErrorStatus(boolean status){
+        errorFound = status;
+    }
+    private void error(List listForErrors, String errorMessage){
+        errorFound = true;
+        listForErrors.add(errorMessage);
+    }
+    private String getErrors(List<String> list){
+        StringBuilder answer = new StringBuilder();
+        for(String mistake: list){
+            answer.append(mistake);
+        }
+        return answer.toString();
+    }
+}
--- implementation/GUI/src/main/java/gui/CreateTicketController.java
+++ implementation/GUI/src/main/java/gui/CreateTicketController.java
@@ -1,4 +1,58 @@
 package gui;
 
+import businesslogic.api.airplane.SeatImpl;
+import businesslogic.api.customer.TicketCreator;
+import businesslogic.api.flight.Flight;
+import datarecords.FlightData;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import persistence.FlightStorageServiceImpl;
+
+import java.util.List;
+
 public class CreateTicketController {
+
+    private final FlightStorageServiceImpl FSSI = new FlightStorageServiceImpl();
+    public AnchorPane AnchorPaneView;
+
+
+    @FXML
+    ComboBox FlightSelector;
+    @FXML
+    TextField SeatRowGet;
+    @FXML
+    TextField SeatColumnGet;
+    @FXML
+    TextField CustomerNameGet;
+    @FXML
+    TextField feedbackField;
+    @FXML
+    Button TicketButton;
+    public void initialize(){
+        List<FlightData> FlightList = FSSI.getAll();
+        for(FlightData flighter: FlightList){
+            FlightSelector.getItems().add(flighter);
+        }
+    }
+
+
+    @FXML
+    protected void onCreateTicketPress(ActionEvent e){
+        //
+        Flight flight = (Flight) FlightSelector.getValue();
+        String seatRow = SeatRowGet.getText();
+        String seatColumn = SeatColumnGet.getText();
+        String customerName = CustomerNameGet.getText();
+        feedbackField.clear();
+        feedbackField.appendText(useInfo(flight, seatRow, seatColumn, customerName));
+    }
+
+    protected String useInfo(Flight f, String row, String column, String name){
+        TicketCreator tc = new TicketCreator();
+        return tc.createTicket(f,row,column,name);
+    }
 }
