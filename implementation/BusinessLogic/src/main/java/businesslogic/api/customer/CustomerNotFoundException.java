b60eb17f74c94ee67fbda6447d522a0472b629eb
Hephaisto-dev <hephaistopl@gmail.com>
2023-04-11 11:11:33 +0200

    Teacher says F

---
+++ implementation/BusinessLogic/src/main/java/businesslogic/api/customer/CustomerNotFoundException.java
@@ -0,0 +1,11 @@
+package businesslogic.api.customer;
+
+public class CustomerNotFoundException extends Exception {
+    public CustomerNotFoundException(String message) {
+        super(message);
+    }
+
+    public CustomerNotFoundException() {
+
+    }
+}
