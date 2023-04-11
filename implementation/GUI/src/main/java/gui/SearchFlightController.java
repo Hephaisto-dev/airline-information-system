2bcd89534259fdcc534fdf9421e7a0b524a20e03
Jazziki17 <jazzfel2222@gmail.com>
2023-04-11 16:29:43 +0200

    No idea

--- implementation/GUI/src/main/java/gui/SearchFlightController.java
+++ implementation/GUI/src/main/java/gui/SearchFlightController.java
@@ -1,5 +1,6 @@
 package gui;
 
+import datarecords.FlightData;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
@@ -7,10 +8,16 @@ import javafx.scene.control.TableView;
 public class SearchFlightController {
 
         @FXML
-        private TableView<?> Table;
+        private TableView<FlightData> Table;
 
         @FXML
-        private TableColumn<?, ?> flightID;
+        private TableColumn<?, String> flightID;
+        private TableColumn<?, String> routeDataFrom;
+        private TableColumn<?, String> routeDataToo0;
+        private TableColumn<?, String> etdDateTime;
+        private TableColumn<?, String> etaDateTime;
+        private TableColumn<?, Integer> flightDuration;
+        private TableColumn<?, String> airplaneId;
 
 
 
