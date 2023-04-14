95a8ada08518f11f2d2b836d8b0dcf0a832941d8
Hephaisto-dev <hephaistopl@gmail.com>
2023-04-14 12:42:18 +0200

    Fix StorageService

--- implementation/Persistence/src/main/java/persistence/api/StorageService.java
+++ implementation/Persistence/src/main/java/persistence/api/StorageService.java
@@ -1,14 +1,14 @@
 package persistence.api;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 public interface StorageService<D extends Record> {
     D add(D data);
 
 
-    default List<D> getAll() {
-        return new ArrayList<>();
+    default Set<D> getAll() {
+        return new HashSet<>();
     }
 
     default boolean remove(String id) {
