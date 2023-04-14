9ac8e681611a53512baa08041359fb589baf7981
LucasWeijs <l.weijs@student.fontys.nl>
2023-04-14 10:10:03 +0200

    moved cancel booking *REAL*

--- implementation/BusinessLogic/src/main/java/businesslogic/api/manager/BookingManager.java
+++ implementation/BusinessLogic/src/main/java/businesslogic/api/manager/BookingManager.java
@@ -8,13 +8,8 @@ import persistence.api.AirportStorageService;
 import persistence.api.BookingStorageService;
 
 public class BookingManager extends ManagerImpl<Booking, BookingData> {
-    private BookingStorageService bookingStorageService;
     public BookingManager(BookingStorageService storageService) {
         super(storageService);
     }
-    public boolean cancelBooking(String id) {
 
-        return bookingStorageService.remove(id);
-
-    }
 }
