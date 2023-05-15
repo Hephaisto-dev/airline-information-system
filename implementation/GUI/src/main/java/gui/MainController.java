c92088942f5144e56e5a182d12181b6a25d69601
MartinIsPresent <m.ivanov@student.fontys.nl>
2023-05-15 12:03:26 +0200

    Added a logout button

--- implementation/GUI/src/main/java/gui/MainController.java
+++ implementation/GUI/src/main/java/gui/MainController.java
@@ -13,12 +13,18 @@ import java.util.function.Supplier;
 
 public class MainController implements Initializable {
 
+    @FXML
+    private Button logout;
+
     private final Supplier<SceneManager> sceneManagerSupplier;
     public MainController(Supplier<SceneManager> sceneManagerSupplier){
         this.sceneManagerSupplier = sceneManagerSupplier;
     }
     //TODO Implement the rest of the code here after discussion with the team
 
+    public void sendToLogin(){
+        sceneManagerSupplier.get().changeScene("loginView");
+    }
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
