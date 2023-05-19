2d6e3af7303bac5eab29564d9557af55396c9783
MartinIsPresent <m.ivanov@student.fontys.nl>
2023-05-19 17:57:59 +0200

    icon removal

--- implementation/GUI/src/main/java/module-info.java
+++ implementation/GUI/src/main/java/module-info.java
@@ -3,7 +3,6 @@ module gui_module {
     requires javafx.fxml;
     requires java.logging;
     requires businesslogic_module;
-    requires fontawesomefx;
     requires java.sql;
     requires java.desktop;
 
