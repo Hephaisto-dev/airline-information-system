5c64abac5f69c3a2c7aa8782659fd1d3a5f8e2d9
MartinIsPresent <m.ivanov@student.fontys.nl>
2023-05-19 13:29:51 +0200

    Removed icons and icon functionality

--- implementation/GUI/src/main/java/gui/FooterController.java
+++ implementation/GUI/src/main/java/gui/FooterController.java
@@ -1,59 +1,10 @@
 package gui;
 
-import de.jensd.fx.glyphs.fontawesome.FontAwesomeIcon;
-import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-
-import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.util.function.Supplier;
-
-//import static jdk.jpackage.internal.WixAppImageFragmentBuilder.ShortcutsFolder.Desktop;
-import java.awt.Desktop;
 public class FooterController {
     private final Supplier<SceneManager> sceneManagerSupplier;
 
-    @FXML
-    private FontAwesomeIcon InstagramIcon;
-
-    @FXML
-    private FontAwesomeIcon emailIcon;
-
-    @FXML
-    private FontAwesomeIcon facebookIcon;
-
-    @FXML
-    private FontAwesomeIcon twitterIcon;
-
     public FooterController(Supplier<SceneManager> sceneManagerSupplier){
         this.sceneManagerSupplier = sceneManagerSupplier;
-    }
-    @FXML
-    public void initialize() {
-        facebookIcon.setOnMouseClicked(event -> {
-            openUrl("https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley");
-        });
-
-        twitterIcon.setOnMouseClicked(event -> {
-            openUrl("https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley");
-        });
-
-        InstagramIcon.setOnMouseClicked(event -> {
-            openUrl("https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley");
-        });
-
-        emailIcon.setOnMouseClicked(event -> {
-            openUrl("https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley");
-        });
-    }
-
-    // helper method to open a URL in the default browser
-    private void openUrl(String url) {
-        try {
-            Desktop.getDesktop().browse(new URI(url));
-        } catch (IOException | URISyntaxException e) {
-            e.printStackTrace();
-        }
     }
 }
