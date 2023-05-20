f409d468e8b11101314827483167fb3324c8808d
Hephaisto-dev <hephaistopl@gmail.com>
2023-05-20 16:23:40 +0200

    Add auto resize when scene is changed

--- implementation/GUI/src/main/java/gui/SceneManager.java
+++ implementation/GUI/src/main/java/gui/SceneManager.java
@@ -81,6 +81,10 @@ public class SceneManager {
 
     void displayOn(Stage stage) {
         stage.setScene(scene);
+        scene.rootProperty().addListener(observable -> {
+            stage.sizeToScene();
+            stage.centerOnScreen();
+        });
         stage.show();
     }
 
