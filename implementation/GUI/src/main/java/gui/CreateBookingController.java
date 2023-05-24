e337006b9f49c3e314d04ea23658a43e87965bd7
LucasWeijs <l.weijs@student.fontys.nl>
2023-05-24 14:00:56 +0200

    update on Gui and made working search

--- implementation/GUI/src/main/java/gui/CreateBookingController.java
+++ implementation/GUI/src/main/java/gui/CreateBookingController.java
@@ -8,6 +8,7 @@ import businesslogic.api.customer.TicketCreator;
 import businesslogic.api.employee.Employee;
 import businesslogic.api.flight.Flight;
 import businesslogic.api.flight.FlightFactory;
+import businesslogic.api.manager.AirportManager;
 import businesslogic.api.manager.BookingManager;
 import businesslogic.api.manager.CustomerManager;
 import businesslogic.api.manager.FlightManager;
@@ -54,6 +55,10 @@ public class CreateBookingController implements Initializable {
     @FXML
     public TextField empId;
     @FXML
+    public TextField tfArri;
+    @FXML
+    public TextField tfDep;
+    @FXML
     public TextField firstName;
     @FXML
     public TextField passengerName;
@@ -78,25 +83,33 @@ public class CreateBookingController implements Initializable {
     @FXML
     public Text totalToPay;
     @FXML
-    public ListView<Flight> lvFlight;
+    public ListView<Flight> lvFlights;
+
+    Collection<Flight> filteredFlights = new ArrayList<Flight>();
+    Collection<Flight> allFlights = new ArrayList<Flight>();
+
     @FXML
     public Text pricePerPerson;
     private BookingCreator bookCreator;
     private FlightManager flightManager;
+    private AirportManager airportManager;
 
 
-    public CreateBookingController(Supplier<SceneManager> sceneManagerSupplier, BookingManager bookingManager, FlightManager flightManager) {
+
+
+    public CreateBookingController(Supplier<SceneManager> sceneManagerSupplier, BookingManager bookingManager, FlightManager flightManager, AirportManager airportManager) {
         this.sceneManagerSupplier = sceneManagerSupplier;
         this.bookingManager = bookingManager;
         this.bookingCreator = new BookingCreator(bookingManager);
         this.flightManager = flightManager;
+        this.airportManager= airportManager;
     }
 
     @FXML
     public void createBooking(ActionEvent actionEvent) {
 
 
-        String booking = bookingCreator.createBooking(cbFlights.getValue().toString()+MainCustomer.firstName()+MainCustomer.lastName(),empId.getText(), cbFlights.getValue().getData(), tickets, LocalDate.now(), extras, customers,MainCustomer);
+        String booking = bookingCreator.createBooking(cbFlights.getValue().toString()+MainCustomer.firstName()+MainCustomer.lastName(),empId.getText(), lvFlights.getSelectionModel().getSelectedItem().getData(), tickets, LocalDate.now(), extras, customers,MainCustomer);
         result.setText(booking);
 
 
@@ -111,6 +124,19 @@ public class CreateBookingController implements Initializable {
         MainCustomer = new CustomerData("CU_" + email.getText(), firstName.getText(), lastName.getText(), dateOfBirth.getValue(), email.getText());
         lblMainCustomer.setText(MainCustomer.firstName()+" "+MainCustomer.lastName());
         btnAddCustomer.setDisable(true);
+    }
+    @FXML
+    public void search(ActionEvent actionEvent){
+        filteredFlights.clear();
+        for(Flight f: allFlights){
+            if(f.getArrival().getName().contains(tfArri.getText())&&f.getDeparture().getName().contains(tfDep.getText())){
+
+                filteredFlights.add(f);
+            }
+        }
+        lvFlights.getItems().clear();
+        lvFlights.getItems().addAll(filteredFlights);
+
     }
     @FXML
     public void addExtra(ActionEvent actionEvent) {
@@ -164,14 +190,8 @@ public class CreateBookingController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        cbExtras.getItems().add("Extra leg space");
-        cbExtras.getItems().add("vegetarian food");
-        cbExtras.getItems().add("Extra luggage");
-        cbExtras.getItems().add("Prepaid food");
-
-        //lvFlight.getItems().addAll(flightManager.getAll());
-        cbFlights.getItems().addAll(flightManager.getAll());
-        Utils.makeComboBoxSearchable(cbFlights, Flight::getId);
+        allFlights = flightManager.getAll();
+        lvFlights.getItems().addAll(allFlights);
         result.setText("");
     }
 }
